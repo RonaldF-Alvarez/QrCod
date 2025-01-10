0752d78832df0cf041c404042de4623c3bb7953f
Rony Dev <110070169+RonaldF-Alvarez@users.noreply.github.com>
2025-01-10 17:33:08 -0300

    20

--- q_r_c_o_d_e_scanner/lib/all_ticket_entradas/all_ticket_entradas_widget.dart
+++ q_r_c_o_d_e_scanner/lib/all_ticket_entradas/all_ticket_entradas_widget.dart
@@ -200,7 +200,7 @@ class _AllTicketEntradasWidgetState extends State<AllTicketEntradasWidget> {
                                                     .postInserteColetorxEntradaCall
                                                     .call(
                                               api: FFAppState().ipadress,
-                                              idColetor: entradaItem.idColetor,
+                                              idColetor: FFAppState().idColetor,
                                               idEntrada: entradaItem.idEntrada,
                                             );
 
@@ -218,7 +218,7 @@ class _AllTicketEntradasWidgetState extends State<AllTicketEntradasWidget> {
                                                     .deleteColetorxEntradasCall
                                                     .call(
                                               api: FFAppState().ipadress,
-                                              idColetor: entradaItem.idColetor,
+                                              idColetor: FFAppState().idColetor,
                                               idEntrada: entradaItem.idEntrada,
                                             );
 
